36098d51336ff937b9b70b22c999732450cebdb5
nadineGomes <54188679+nadineGomes@users.noreply.github.com>
2020-04-11 14:49:11 -0300

    Add files via upload

---
+++ area/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseX=0, alturaX=0;
+
+            Console.WriteLine("Calculadora - Area do Triângulo");
+            
+            Console.Write("Digite o Valor da Base: ");
+            baseX = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura: ");
+            alturaX = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A Area do triângulo é " + ((baseX*alturaX)/2));
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
